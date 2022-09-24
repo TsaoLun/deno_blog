c1b10afc90939785e787b64e0f6d3735c3a6b8a3
TsaoLun <ycl18@live.com>
2022-09-24 23:35:54 +0800

    fix not refresh problem after delete a markdown file

--- init.ts
+++ init.ts
@@ -71,7 +71,7 @@ const DENO_JSONC_CONTENTS = `{
 const IMPORT_MAP_JSON_NAME = "import_map.json";
 const IMPORT_MAP_JSON_CONTENTS = `{
   "imports": {
-    "blog": "https://deno.land/x/deno_blog@v0.1.0/blog.tsx"
+    "blog": "https://deno.land/x/deno_blog@v0.1.1/blog.tsx"
   }
 }
 `;
